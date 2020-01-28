a51dac296160bbbe553d31fc9616f112f02a8205
pol <polcaes@gmail.com>
2020-01-28 10:46:57 +0100

    PomaMSnSetClass

---
+++ R/PomaMSnSetClass.R
@@ -0,0 +1,27 @@
+
+#' Convert data frames to an MSnSet Object
+#'
+#' @param target Metadata variables structured in columns. Sample ID must be the first column and group/type of study must be the second column.
+#' @param features Table of features. Each feature in one column.
+#'
+#' @export
+#'
+#' @return An MSnSet object.
+#' @references Laurent Gatto and Kathryn S. Lilley. MSnbase - an R/Bioconductor package for isobaric tagged mass spectrometry data visualization, processing and quantitation. Bioinformatics 28, 288-289 (2012).
+#' @author Pol Castellano-Escuder
+#'
+#' @importFrom MSnbase MSnSet
+#' @importFrom tibble column_to_rownames
+PomaMSnSetClass <- function(target, features){
+
+  target <- as.data.frame(target)
+  colnames(target)[1] <- "ID"
+  target <- column_to_rownames(target, "ID")
+
+  data <- MSnbase::MSnSet(exprs = t(features), pData = target)
+
+  if (validObject(data))
+    return(data)
+
+}
+
---
+++ tests/testthat/test-PomaMSnSetClass.R
@@ -0,0 +1,20 @@
+context("PomaMSnSetClass")
+
+test_that("PomaMSnSetClass works", {
+
+  target <- data.frame(ID = c("One", "Two", "Three", "Four"), Group = c("Trtd", "Ctrl", "Trtd", "Ctrl"), Smoking = c(1,0,0,1))
+  target2 <- data.frame(ID = c("Five", "One", "Three", "Two"), Group = c("Ctrl", "Trtd", "Trtd", "Ctrl"), Smoking = c(0,0,0,1))
+  features <- data.frame(feat1 = c(1,2,3,4), feat2 = c(6,3,7,3), feat3 = c(3,5,23,24))
+  features_error <- data.frame(feat1 = c(1,2,3,4,5), feat2 = c(6,3,7,4,3), feat3 = c(3,4,5,23,24))
+
+  a <- PomaMSnSetClass(target, features)
+  b <- PomaMSnSetClass(target2, features)
+
+  expect_true(validObject(a))
+  expect_true(validObject(b))
+
+  expect_error(PomaMSnSetClass(target, features_error))
+  expect_error(PomaMSnSetClass(target))
+  expect_error(PomaMSnSetClass(features))
+
+})
